df46e36f122e73e248e268925a15e0614caec676
bowonShin <bowo9700@Gamil.com>
2026-05-05 21:11:09 +0900

    fix: FOnPickUp
    - FOnPickUp 같은 이름이 있어서 수정합니다.

--- Source/Ch3_Team2/PickUpComp.h
+++ Source/Ch3_Team2/PickUpComp.h
@@ -9,7 +9,8 @@
 
 // Declaration of the delegate that will be called when someone picks this up
 // The character picking this up is the parameter sent with the notification
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPickUp, AAPlayer*, PickUpCharacter);
+// FOn_PickUp 형태 이름 선언이기 떄문에 다른 파일에 있는 이름과 중복되지 않도록 설정할것
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOn_PickUp, AAPlayer*, PickUpCharacter);
 
 UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class CH3_TEAM2_API UPickUpComp : public USphereComponent
@@ -17,8 +18,8 @@ class CH3_TEAM2_API UPickUpComp : public USphereComponent
 	GENERATED_BODY()
 public:
 	/** Delegate to whom anyone can subscribe to receive this event */
-	UPROPERTY(BlueprintAssignable, Category = "Interaction")
-	FOnPickUp OnPickUp;
+	UPROPERTY(BlueprintAssignable, Category = Interaction)
+	FOn_PickUp OnPickUp;
 	UPickUpComp();
 protected:
 	
