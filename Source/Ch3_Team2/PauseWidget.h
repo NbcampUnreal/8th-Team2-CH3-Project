4179dd386760ba9db519ab99a5ea1c412ce6279e
junahak <junahak0019@gmail.com>
2026-05-07 17:40:59 +0900

    ModalLayer

---
+++ Source/Ch3_Team2/PauseWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "PauseWidget.generated.h"
+
+UCLASS()
+class CH3_TEAM2_API UPauseWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPauseWidget(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	// 위젯이 화면에 켜질 때 (스택에 추가될 때)
+	virtual void NativeOnActivated() override;
+
+	// 위젯이 화면에서 꺼질 때 (스택에서 제거될 때)
+	virtual void NativeOnDeactivated() override;
+
+	// 입력 모드 설정 (UI 포커스)
+	virtual TOptional<FUIInputConfig> GetDesiredInputConfig() const override;
+};
