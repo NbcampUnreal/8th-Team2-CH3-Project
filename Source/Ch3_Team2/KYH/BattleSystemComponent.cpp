e5e5132ae1abec4d268b0730ef8f5902564b474c
YH760 <opqwoiwr@gmail.com>
2026-05-11 15:55:09 +0900

    feat: BattleSystemComponent
    - 전투 결과 방송을 위한 델리게이트 구현
    - 게임 매니저 블루프린트에 배틀 시스템 컴포넌트 부착
    - 시스템 검증을 위한 레벨 생성

--- Source/Ch3_Team2/KYH/BattleSystemComponent.cpp
+++ Source/Ch3_Team2/KYH/BattleSystemComponent.cpp
@@ -15,30 +15,39 @@ void UBattleSystemComponent::BeginPlay()
 	Super::BeginPlay();
 }
 
-/*
-void UBattleSystemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-}
-*/
 
-void UBattleSystemComponent::ExecuteDamageCalculation(AActor* Attacker, AActor* Victim, float BaseDamage)
+//void UBattleSystemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+//{
+//	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+//}
+
+
+void UBattleSystemComponent::ExecuteDamageCalculation(AActor* Attacker, AActor* Victim, float BaseDamage, bool bIsCritical, float CritMultiplier)
 {
 	if (!Attacker || !Victim) return;
 	
 	float FinalDamage = BaseDamage;
+    
+    if (bIsCritical && Attacker->ActorHasTag(FName("Player")))
+    {
+       FinalDamage = BaseDamage * CritMultiplier;
+    }
 	
 	Victim->TakeDamage(FinalDamage, FDamageEvent(), Attacker->GetInstigatorController(), Attacker);
 	
+	if (OnBattleResult.IsBound())
+    {
+        OnBattleResult.Broadcast(Attacker, Victim, FinalDamage, bIsCritical);
+    }
+
+	// DamageLog
 	if (GEngine)
 	{
 		FString Message = FString::Printf(TEXT("[%s] -> [%s] : %.f Damage!"), 
 			*Attacker->GetName(), *Victim->GetName(), FinalDamage);
 	        
-		// 5초 동안, 하늘색으로, 화면 상단에 출력
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, Message);
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, Message);
 	}
-	
 	UE_LOG(LogTemp, Log, TEXT("=== BATTLE REPORT ==="));
 	UE_LOG(LogTemp, Log, TEXT("Attacker: %s"), *Attacker->GetName());
 	UE_LOG(LogTemp, Log, TEXT("Victim: %s"), *Victim->GetName());
--- Source/Ch3_Team2/KYH/BattleSystemComponent.h
+++ Source/Ch3_Team2/KYH/BattleSystemComponent.h
@@ -6,6 +6,13 @@
 #include "Components/ActorComponent.h"
 #include "BattleSystemComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(
+	FOnBattleResultSignature,
+	AActor*, Attacker,
+	AActor*, Victim,
+	float, FinalDamage,
+	bool, bIsCritical);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CH3_TEAM2_API UBattleSystemComponent : public UActorComponent
 {
@@ -14,13 +21,18 @@ class CH3_TEAM2_API UBattleSystemComponent : public UActorComponent
 public:	
 	UBattleSystemComponent();
 
-protected:
-	virtual void BeginPlay() override;
-
-public:	
-	// virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(BlueprintAssignable, Category = "Battle")
+	FOnBattleResultSignature OnBattleResult;
 
 	UFUNCTION(BlueprintCallable, Category = "Battle")
-	void ExecuteDamageCalculation(AActor* Attacker, AActor* Victim, float BaseDamage);
+	void ExecuteDamageCalculation(AActor* Attacker, AActor* Victim, float BaseDamage, bool bIsCritical, float CritMultiplier);
+	
+protected:
+	virtual void BeginPlay() override;
+	// virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:	
+	
+
 	
 };
