910cd4ebf29d56827e4336b20a72fd5ab05839e3
bowonShin <bowo9777@naver.com>
2026-05-14 21:13:54 +0900

    imporce: SkillRadialExplosion
    - RadialExplosion 스킬 추가
    ( 넉백 스킬 )

---
+++ Source/Ch3_Team2/SBW/SkillRadialExplosion.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkillBaseComp.h"
+#include "SkillRadialExplosion.generated.h"
+
+/**
+ * 
+ */
+class UCollisionCompoent;
+
+UCLASS()
+class CH3_TEAM2_API USkillRadialExplosion : public USkillBaseComp
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category= "SkillStat")
+	float DetectionRadius = 1000.0f; // 인식 범위
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category= "SkillStat")
+	float LaunchZDirection = 0.2f; // 넉백시 살짝 밀려나는 z값
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category= "SkillStat")
+	float KnockbackStrength = 600.0f;// 밀려나는 힘 (수치 조절 필요)
+	
+	virtual void SkillActive()override;
+};
